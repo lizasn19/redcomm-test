15742a849e9826cf3a83281033a1a8c58cac4fe5
Siti Nurhalizah <lizasn19@gmail.com>
2026-06-26 13:03:46 +0700

    Make apiBase configurable via env variables for Vercel deployment

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -7,7 +7,7 @@ export default defineNuxtConfig({
   css: ['~/assets/css/main.css'],
   runtimeConfig: {
     public: {
-      apiBase: 'http://localhost:8000/api'
+      apiBase: process.env.NUXT_PUBLIC_API_BASE || 'http://localhost:8000/api'
     }
   }
 })
